91bbe0bbd117109a5b3d9b03e9455d7a903ff227
cr33p1ngp4ck3t <86455561+cr33p1ngp4ck3t@users.noreply.github.com>
2025-10-15 22:05:36 +0500

    Revert "feat: Implement Image Optimization (LQIP)"

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,15 +1,22 @@
 import imageUrlBuilder from "@sanity/image-url";
 import { client } from "./client";
+import { getPlaiceholder } from "plaiceholder";
 
 const builder = imageUrlBuilder(client);
 
 interface SanityImageSource {
-  asset: {
-    _ref: string;
-    _type: string;
-  };
+  asset: string;
 }
 
-export function urlFor(source: SanityImageSource) {
-  return builder.image(source);
+export async function urlFor(source: SanityImageSource) {
+  const imageUrl = builder.image(source).width(800).height(400).url();
+  const buffer = await fetch(imageUrl).then(async (res) =>
+    Buffer.from(await res.arrayBuffer())
+  );
+  const { base64 } = await getPlaiceholder(buffer);
+
+  return {
+    src: imageUrl,
+    blurDataURL: base64,
+  };
 }
